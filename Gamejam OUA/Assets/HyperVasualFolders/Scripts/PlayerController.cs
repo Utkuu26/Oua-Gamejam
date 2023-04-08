d7742732fb209872aa77b131582c21270fe41d6a
Halilibrahim Karaoğlan <halilibrahimkaraoglan92@gmail.com>
2023-04-08 10:03:52 +0300

    Yeni  Scriptler Eklendi
    Camera Controller yazıldı, Karakter Controlleri geliştirildi ve Horizontal ayarları yapıldı.

--- Gamejam OUA/Assets/HyperVasualFolders/Scripts/PlayerController.cs
+++ Gamejam OUA/Assets/HyperVasualFolders/Scripts/PlayerController.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float runningSpeed;
-    
+    float touchXDelta = 0;
+    float newX = 0;
+    public float XSpeed;
+    public float limitx;
+
     void Start()
     {
-        
+
     }
 
-    
+
     void Update()
     {
-        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + runningSpeed * Time.deltaTime);
+        SwipeCheck();
+    }
+    private void SwipeCheck()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            //Debug.Log(Input.GetAxis("Horizontal"));
+            touchXDelta = Input.GetAxis("Horizontal") / (Screen.width * 10);
+        }
+
+        newX = transform.position.x + XSpeed * touchXDelta + Time.deltaTime;
+        newX = Mathf.Clamp(newX, -limitx, limitx);
+
+
+        Vector3 newPosition = new Vector3(newX, transform.position.y, transform.position.z + runningSpeed * Time.deltaTime);
         transform.position = newPosition;
     }
 }
