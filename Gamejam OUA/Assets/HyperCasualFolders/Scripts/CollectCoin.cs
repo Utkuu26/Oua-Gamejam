dcf6e8668f1194ec15e63980b4d3d2277ac11fb3
Halilibrahim Karaoğlan <halilibrahimkaraoglan92@gmail.com>
2023-04-08 12:58:22 +0300

    Yeni engel enkelndi ve reload scene yapıldı
    Engele çarpınca sahnenin yeniden yüklenmesi yazıldı.

--- Gamejam OUA/Assets/HyperCasualFolders/Scripts/CollectCoin.cs
+++ Gamejam OUA/Assets/HyperCasualFolders/Scripts/CollectCoin.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CollectCoin : MonoBehaviour
 {
     public int score;
     public TextMeshProUGUI CoinText;
+    public PlayerController PlayerController;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
@@ -14,12 +17,18 @@ public class CollectCoin : MonoBehaviour
             AddCoin();
             Destroy(other.gameObject);
         }
+        else if (other.CompareTag("End"))
+        {
+            Debug.Log("congrats");
+            PlayerController.runningSpeed = 0;
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Collison"))
         {
             Debug.Log("Deðdi");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
     public void AddCoin()
