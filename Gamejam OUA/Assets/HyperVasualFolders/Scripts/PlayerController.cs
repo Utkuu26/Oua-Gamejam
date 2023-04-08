38e2058d9e08c7731210b3785eae00b79855f9ca
Halilibrahim Karaoğlan <halilibrahimkaraoglan92@gmail.com>
2023-04-08 11:23:54 +0300

    Coinler ve ilk engel eklendi
    Coinler eklendi ve score sistemi yazıldı. ilk engel yerleştirildi

--- Gamejam OUA/Assets/HyperVasualFolders/Scripts/PlayerController.cs
+++ Gamejam OUA/Assets/HyperVasualFolders/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+ 
     public float runningSpeed;
     float touchXDelta = 0;
     float newX = 0;
@@ -22,14 +23,16 @@ public class PlayerController : MonoBehaviour
     }
     private void SwipeCheck()
     {
+        
         if (Input.GetAxis("Horizontal") != 0)
         {
             //Debug.Log(Input.GetAxis("Horizontal"));
             touchXDelta = Input.GetAxis("Horizontal") / (Screen.width * 10);
+            newX = transform.position.x + XSpeed * touchXDelta + Time.deltaTime;
+            newX = Mathf.Clamp(newX, -limitx, limitx);
         }
 
-        newX = transform.position.x + XSpeed * touchXDelta + Time.deltaTime;
-        newX = Mathf.Clamp(newX, -limitx, limitx);
+       
 
 
         Vector3 newPosition = new Vector3(newX, transform.position.y, transform.position.z + runningSpeed * Time.deltaTime);
